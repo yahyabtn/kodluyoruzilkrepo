7afad66a80ce4e6de9217f76a32a838029a99d0f
yahyabtn <111643509+yahyabtn@users.noreply.github.com>
2022-09-12 15:48:37 +0300

    Add files via upload

---
+++ ForDongusu.java
@@ -0,0 +1,17 @@
+import javax.net.ssl.SSLContext;
+
+public class ForDongusu {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+
+        int i = 1;
+        while (i <= 10) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
