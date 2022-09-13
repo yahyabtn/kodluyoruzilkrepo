9acb236eecf269298f4eb55402ea322caa2b4224
yahyabtn <111643509+yahyabtn@users.noreply.github.com>
2022-09-13 13:32:40 +0300

    Add files via upload

---
+++ Harmonikseri.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Harmonikseri {
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+        System.out.println("N sayısını giriniz : ");
+        int n = input.nextInt();
+        double result = 0.0;
+        for(double i = 1; i<= n; i++) {
+            result += (1/i);
+        }
+        System.out.println(result);
+    }
+}
